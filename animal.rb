6b5fb09d7ba68503382cb47ad91c60bdf8053624
Amourlive <aleksei_mackevich@outlook.com>
2018-03-16 17:25:52 +0300

    create Superclass animal

---
+++ animal.rb
@@ -0,0 +1,26 @@
+# Superclass
+class Animal
+  attr_reader :name, :age
+
+  def name=(value)
+    raise 'Name can\'t be blank' if value == ''
+    @name = value
+  end
+
+  def age=(value)
+    raise "An age of #{value}" if value < 0
+    @age = value
+  end
+
+  def talk
+    puts "#{name} says Bark!"
+  end
+
+  def move(destination)
+    puts "#{name} runs to the #{destination}"
+  end
+
+  def report_age
+    puts "#{name} is #{age} years old"
+  end
+end
