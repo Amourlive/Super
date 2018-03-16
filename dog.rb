1a4ca4f3095010ed83583082d075143102f43db6
Amourlive <aleksei_mackevich@outlook.com>
2018-03-16 17:32:27 +0300

    Add class dog, added functionality into method #move

--- dog.rb
+++ dog.rb
@@ -1,4 +1,12 @@
 load 'animal.rb'
 # Class Dog
 class Dog < Animal
+  def move(destination)
+    puts "#{@name} scratches his ear"
+    super
+  end
 end
+
+dillon = Dog.new
+dillon.name = 'Dillon'
+dillon.move('tree')
